292b748c37572f17523d12c8b27329550bc7f65c
ConorFeeD <kirillkostuk71@gmail.com>
2022-11-17 11:16:02 +0300

    lesson 6

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 
 class News(models.Model):
@@ -8,3 +10,26 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to='photos/%Y/%m%d/')
     is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
