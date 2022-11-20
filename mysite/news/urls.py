0dd260b39fd5a25e950d2aa55ad1fb3c0466b26b
ConorFeeD <kirillkostuk71@gmail.com>
2022-11-20 14:19:10 +0300

    lesson 8

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     path('', index),
     path('test/', test),
+    path('<int:question_id>/', detail, name='detail'),
+    path('<int:question_id>/results/', results, name='results'),
+    path('<int:question_id>/vote/', vote, name='vote'),
 ]
