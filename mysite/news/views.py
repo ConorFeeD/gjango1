70f0b30eacc2141c7fffcd664d9fee5adfa85783
ConorFeeD <kirillkostuk71@gmail.com>
2022-11-20 16:57:45 +0300

    lesson 8

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,21 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Question
+from .models import *
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'news.detail.html', {"question": question})
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
+    response = "You're looking at the results of question results %s."
     return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    return HttpResponse("You're voting on question vote %s." % question_id)
 
 
 def index(request):
@@ -24,7 +25,7 @@ def index(request):
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(request, context))
+    return render(request, 'news/index.html', context)
 
 
 def test(request):
